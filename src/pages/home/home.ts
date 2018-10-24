6006c0f898c3eaab62f8be0ed8ebce0b85193ec4
vithort <vithort@gmail.com>
2018-10-24 15:22:17 -0300

    Update
    Tutorial 18

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  inputtext: string;
+  key: string = "username";
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController
+    , private storage: Storage
+  ) {
 
+  }
+
+  saveData() {
+    this.storage.set(this.key, this.inputtext);
+  }
+
+  loadData() {
+    this.storage.get(this.key).then((val) => {
+      console.log('Your username is', val);
+    });
   }
 
 }
